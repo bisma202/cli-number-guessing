19f49180c18b26d987b08412366e2c527755688b
bisma202 <164556401+muzamil8888@users.noreply.github.com>
2024-03-26 13:20:03 -0700

    meaning full

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-// 1) computer will generate a random number
+// 1) computer will generate a random number-done
 // 2) user input for guessing number
 // 3) compare user input with computer generater number and show result
 const randomNumber = Math.floor(Math.random() * 6 + 1);
